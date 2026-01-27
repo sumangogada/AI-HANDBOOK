bb91f3a55309d3ec5eeb3c619fb50bcd3e49cea8
sumangogada <gogada.suman@gmail.com>
2026-01-27 16:03:59 -0500

    ChatCompletions

---
+++ ChatCompletionsAPI.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+key = os.environ.get('OPENAI_API_KEY')
+client = OpenAI(api_key= key)
+
+# --------------------------------------------------------------
+# Basic text example with the Chat Completions API
+# --------------------------------------------------------------
+
+response = client.chat.completions.create(
+    model="gpt-5.2",
+    messages=[
+        {
+            "role": "user",
+            "content": "How is weather in rhode island today?",
+        },
+        {
+            "role": "developer",
+            "content": "How are you doing ?",
+        }
+    ],
+)
+
+print(response.choices[0].message.content)
+
+response1 = client.responses.create(
+    model="gpt-5.2",input="Write a one-sentence bedtime story about a unicorn"
+)
+
+print(response1.output_text)
