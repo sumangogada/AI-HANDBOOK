0988ee7f88f3398a4725ace189b7057c6d34cd41
sumangogada <gogada.suman@gmail.com>
2026-01-29 10:16:52 -0500

    Restructuring

---
+++ responses/function-call.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+key= os.environ.get('OPENAI_API_KEY')
+client = OpenAI(api_key=key)
+
+tools = [
+    {
+        "type":"function",
+        "name":"send_email",
+        "description":"Send an email to a given recipient with a subject and message.",
+        "parameters":{
+            "type":"object",
+            "properties":{
+                "to":{"type":"string","description":"The recipeint email adress."},
+                "subject": {"type": "string", "description": "Email subject line."},
+                "body": {"type": "string", "description": "Body of the email message."},
+            },
+            "required": ["to", "subject", "body"],
+            "additionalProperties": False,
+        },
+    }
+]
+
+response = client.responses.create(
+    model="gpt-4o",
+    input="Can you send an email to ilan@example.com and katia@example.com saying hi?",
+    tools=tools,
+)
+
+print(response.output)
+print(response.output[0].model_dump_json(indent=2))
+print(response.output[1].model_dump_json(indent=2))
